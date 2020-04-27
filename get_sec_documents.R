884d85efb2b93c153f93c5d812956974b165ad07
Jed Gore <jed.gore@jgore-16mpb-00928.home>
2020-04-27 05:47:24 -0700

    commenting.

--- get_sec_documents.R
+++ get_sec_documents.R
@@ -19,31 +19,31 @@ library(readr)
 #str_controls <-'controls'
 #str_risk <- 'risk factors'
 
-tickers <- read_csv("test_ticker_list.csv")
+df_tickers <- read_csv("test_ticker_list.csv")
 
-get_filings_links <-function(ticker) {
-  filings <- company_filings(ticker, type = "10-", count = 20)
-  filings <- filings[filings$type == "10-K" | filings$type == "10-Q", ]
-  filing_infos <- map_df(filings$href, filing_information)
-  filings <- bind_cols(filings, filing_infos)
-  return(as_tibble(filings))
+get_filings_links <-function(str_ticker) {
+  df_filings <- company_filings(str_ticker, type = "10-", count = 20)
+  df_filings <- df_filings[df_filings$type == "10-K" | df_filings$type == "10-Q", ]
+  df_filing_infos <- map_df(df_filings$href, filing_information)
+  df_filings <- bind_cols(df_filings, df_filing_infos)
+  return(as_tibble(df_filings))
 }
 
-get_section_text <- function(href) {
-  df_filing_documents <- filing_documents(href)
-  doc_href <- df_filing_documents[df_filing_documents$type == "10-K" | df_filing_documents$type == "10-Q",]$href
-  doc <- parse_filing(doc_href)
-  txt <- doc[grepl("discussion", doc$item.name, ignore.case = TRUE), ] # only discussion for now
+get_section_text <- function(str_href) {
+  df_filing_documents <- filing_documents(str_href)
+  str_doc_href <- df_filing_documents[df_filing_documents$type == "10-K" | df_filing_documents$type == "10-Q",]$href
+  doc <- parse_filing(str_doc_href)
+  df_txt <- doc[grepl("discussion", doc$item.name, ignore.case = TRUE), ] # only discussion for now
   #we could do some text preprocessing here.
-  return(as_tibble(txt))
+  return(as_tibble(df_txt))
 }
 
-get_mdna_file <- function(ticker) {
+get_mdna_file <- function(str_ticker) {
   start_time <- Sys.time()
-  print(ticker)
-  filings <- get_filings_links(ticker)
+  print(str_ticker)
+  df_filings <- get_filings_links(str_ticker)
   
-  data <- filings %>% rowwise() %>%
+  df_data <- filings %>% rowwise() %>%
     mutate(mdna = map(href, get_section_text)) %>%
     ungroup() %>%
     select(period_date,filing_date,type,form_name,documents,mdna) %>%
@@ -54,9 +54,9 @@ get_mdna_file <- function(ticker) {
   
   end_time <- Sys.time()
   print(end_time - start_time)
-  return(data)
+  return(df_data)
 }
 
-data <- map_df(tickers$Symbol, get_mdna_file)
+df_data <- map_df(df_tickers$Symbol, get_mdna_file)
 #munge from here.  probably in python.
 
