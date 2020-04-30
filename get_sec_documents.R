a062cb98d7f5337d6e52fcf89a722ba77307f95f
Jed Gore <jed.gore@jgore-16mpb-00928.home>
2020-04-29 21:43:09 -0400

    added other sections

--- get_sec_documents.R
+++ get_sec_documents.R
@@ -26,41 +26,69 @@ get_filings_links <-function(str_ticker) {
   df_filings <- df_filings[df_filings$type == "10-K" | df_filings$type == "10-Q", ]
   df_filing_infos <- map_df(df_filings$href, filing_information)
   df_filings <- bind_cols(df_filings, df_filing_infos)
-  return(as_tibble(df_filings))
+  return(head(as_tibble(df_filings),6))
 }
 
-get_section_text <- function(str_href) {
+get_section_text <- function(str_href, str_section, str_search) {
   df_filing_documents <- filing_documents(str_href)
   str_doc_href <- df_filing_documents[df_filing_documents$type == "10-K" | df_filing_documents$type == "10-Q",]$href
   doc <- parse_filing(str_doc_href)
-  df_txt <- doc[grepl("discussion", doc$item.name, ignore.case = TRUE), ] # only discussion for now
+
+  #print(str_section)
+  
+  df_txt <- doc[grepl(str_section, doc$item.name, ignore.case = TRUE) & grepl(str_search, doc$item.name, ignore.case = TRUE), ] # only discussion for now
   #we could do some text preprocessing here.
+  df_txt$section <- str_section
   return(as_tibble(df_txt))
 }
 
-get_mdna_file <- function(str_ticker) {
+get_document_text <- function(str_ticker) {
   start_time <- Sys.time()
+  
   print(str_ticker)
+  
   df_filings <- get_filings_links(str_ticker)
   
-  df_data <- df_filings %>% rowwise() %>%
-    mutate(mdna = map(href, get_section_text)) %>%
+  df_data <- (df_filings) %>% 
+    rowwise() %>%
+    mutate(nest_discussion = map(.x = href, str_section = 'item 2|item 7',str_search = 'discussion', .f = get_section_text)) %>%
+    mutate(nest_qualitative = map(.x = href, str_section = 'item 3|item 7a', str_search = 'qualitative', .f = get_section_text)) %>%
+    mutate(nest_controls = map(.x = href, str_section = 'item 4|item 9a',str_search = 'controls', .f = get_section_text)) %>%
+    mutate(nest_risk = map(.x = href, str_section = 'item 1',str_search = 'risk factors', .f = get_section_text)) %>%
     ungroup() %>%
-    select(period_date,filing_date,type,form_name,documents,mdna) %>%
+    select(period_date,filing_date,type,form_name,documents,nest_discussion,nest_qualitative,nest_controls,nest_risk) %>%
     group_by(period_date) %>%
-    arrange(desc(period_date)) %>%
-    unnest(cols=c(mdna))
+    arrange(desc(period_date))
+    
+  df_data %>%
+    unnest(cols=c(nest_discussion)) %>%
+    select(-nest_qualitative,-nest_controls,-nest_risk) %>%
+    write_csv(paste0(str_ticker,"_discussion.csv"))
+
+  df_data %>%
+      unnest(cols=c(nest_qualitative)) %>%
+      select(-nest_discussion,-nest_controls,-nest_risk) %>%
+      write_csv(paste0(str_ticker,"_qualitative.csv"))
   
   df_data %>%
-    write_csv(paste0(str_ticker,'.csv'))
-  
+      unnest(cols=c(nest_controls)) %>%
+      select(-nest_discussion,-nest_qualitative,-nest_risk) %>%
+      write_csv(paste0(str_ticker,"_controls.csv"))
+    
+  df_data %>%
+      unnest(cols=c(nest_risk)) %>%
+      select(-nest_discussion,-nest_controls,-nest_qualitative) %>%
+      write_csv(paste0(str_ticker,"_risk.csv"))
+      
   end_time <- Sys.time()
   print(end_time - start_time)
   return(df_data)
 }
 
-df_data <- map_df(df_tickers$Symbol, get_mdna_file)
-#munge from here.  probably in python.
+df_data <- map_df(df_tickers$Symbol, get_document_text)
 
-x <- get_mdna_file("AXP")
-x
+#df_data <- get_document_text('AXP')
+#df_data %>%
+#  select(type,nest_discussion,nest_qualitative,nest_controls,nest_risk) %>%
+#  tail()
+#df_data
