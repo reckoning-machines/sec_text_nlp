be2e86a2bd80af72cdcea0e1a4939caf982ee7fc
Jed Gore <jed.gore@jgore-16mpb-00928.home>
2020-05-01 13:05:28 -0400

    cleanup

--- get_sec_documents.R
+++ get_sec_documents.R
@@ -13,17 +13,9 @@ library(readr)
 #library(log4r) TODO logging file.
 library(googlesheets4)
 
+#tickers list
 str_sheet <- "1_xcDVKjR2jqE-w5LqxIWnrDYstpSrE5nFSSY0WXNOVE"
 df_tickers <- read_sheet(str_sheet)
-
-#HOME_DIR <- getwd()
-#DATA_DIR <- paste0(HOME_DIR,"/DATA/")
-#dir.create('/data') #doesn't work on the mac .. .w out chmod stuff TODO
-
-#df_tickers <- read_csv("test_ticker_list.csv")
-#df_tickers <- drive_download("~/sec_getter/test_ticker_list.csv",type="csv",overwrite = TRUE)
-#df_tickers <- read_csv("test_ticker_list.csv") %>%
-#  filter(Symbol != "BAC")
 
 get_filings_links <-function(str_ticker) {
   df_filings <- company_filings(str_ticker, type = "10-", count = 20)
@@ -52,18 +44,10 @@ get_document_text <- function(str_ticker, force = FALSE) { #not using force yet
   
   print(str_ticker)
   
-  #str_write_file <- paste0(DATA_DIR,str_ticker,"_sec_text.csv")
-  str_write_name <- paste0(str_ticker,"_sec_text")
+  str_write_name <- paste0("~/data/",str_ticker,"_sec_text")
 
-  
-  #if (file.exists(str_write_file)) {
-  #  return(NULL)
-  #}
-  
   df_filings <- get_filings_links(str_ticker)
 
-  #print(href)
-  
   df_data <- (df_filings) %>% 
     rowwise() %>%
     mutate(nest_discussion = map(.x = href, str_section = 'item 2|item 7',str_search = 'discussion', .f = get_section_text)) %>%
@@ -91,50 +75,16 @@ get_document_text <- function(str_ticker, force = FALSE) { #not using force yet
   
   ss <- gs4_create(str_write_name)
   
-#  df_data <- rbind(a,b,c,d) %>%
-#    write_csv(str_write_file) 
-  
   df_data <- rbind(a,b,c,d) %>%
     googlesheets4::sheet_write(ss, sheet = "sec_data")
-  
-  #df_data %>%
-  #  unnest(cols=c(nest_discussion)) %>%
-  #  select(-nest_qualitative,-nest_controls,-nest_risk) %>%
-  #  write_csv(paste0(str_ticker,"_discussion.csv")) 
-
-
-  #df_data %>%
-  #  unnest(cols=c(nest_qualitative)) %>%
-  #  select(-nest_discussion,-nest_controls,-nest_risk) %>%
-  #  write_csv(paste0(str_ticker,"_qualitative.csv")) 
-
-  #df_data %>%
-  #  unnest(cols=c(nest_controls)) %>%
-  #  select(-nest_discussion,-nest_qualitative,-nest_risk) %>%
-  #  write_csv(paste0(str_ticker,"_controls.csv")) 
-    
-  #print("four")
-  #df_data %>%
-  #  unnest(cols=c(nest_risk)) %>%
-  #  select(-nest_discussion,-nest_controls,-nest_qualitative) %>%
-  #  write_csv(paste0(str_ticker,"_risk.csv")) 
   
   end_time <- Sys.time()
   print(end_time - start_time)
   return(df_data)
 }
 
-#upload_files <- function(str_ticker, force = FALSE) { #too slow to be useful
-#  print(str_ticker)
-#  drive_upload(paste0(getwd(),"/",str_ticker,"_discussion.csv"),path = paste0("~/sec_getter/data/",str_ticker,"_discussion.csv"),overwrite = TRUE) 
-#  drive_upload(paste0(getwd(),"/",str_ticker,"_qualitative.csv"),path = paste0("~/sec_getter/data/",str_ticker,"_qualitative.csv"),overwrite = TRUE) 
-#  drive_upload(paste0(getwd(),"/",str_ticker,"_controls.csv"),path = paste0("~/sec_getter/data/",str_ticker,"_controls.csv"),overwrite = TRUE) 
-#  drive_upload(paste0(getwd(),"/",str_ticker,"_risk.csv"),path = paste0("~/sec_getter/data/",str_ticker,"_risk.csv"),overwrite = TRUE) 
-#}
-
 #long run.
 df_data <- map_df(df_tickers$Symbol, get_document_text)
 
-#map(df_tickers$Symbol, upload_files)
 
 
