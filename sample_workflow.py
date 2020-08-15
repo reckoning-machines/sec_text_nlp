71ce52e7638a9b7fa46796d629333ea05b1ee5c6
Jed Gore <jed.gore@jgore-16mpb-00928.home>
2020-08-15 17:33:04 -0400

    sample workflow

--- sample_workflow.py
+++ sample_workflow.py
@@ -1,42 +1,38 @@
-
-
 from sec_text_nlp import *
-#TICKER_LIST = 'test_ticker_list.csv'
 
-#SECTION or TOPIC modeling
-#revenue recognition
-#join venture
-#related party
-#acquisitions
-#commissions
-#receivables
-#dividends
+wiki = wiki_sp500()
 
-stn = SECTextNLP("AAPL")
-print(stn.df_file_index.columns)
-print(stn.df_file_index)
-print(stn.df_text)
-#print(pd.merge(stn.df_text,stn.df_file_index,how = 'inner',left_on='href',right_on='href')[['ticker','filing_date','sentence_text']])
-#print(pd.merge(stn.df_mdna,stn.df_file_index,how = 'inner',left_on='href',right_on='href')[['ticker','filing_date','sentence_text']])
-
-#print('business segments')
-#print(stn.get_noun_phrases_around_topic(BUSINESS_SEGMENT_LIST))
-#print('products list')
-#print(stn.get_noun_phrases_around_topic(PRODUCTS_LIST,nrows=2))
-#print('trademarks')
-#print(stn.get_words_with_trademark(stn.df_mdna))
-
-#df = stn.match_keywords(stn.NLTK_sentiment(stn.df_mdna),GLOBAL_SEARCH_LIST,'MACRO')
-#print(df)
-
-#list_trademarks = stn.get_words_with_trademark(stn.df_mdna)
-#df = stn.match_keywords(stn.NLTK_sentiment(stn.df_mdna),list_trademarks,'trademarks')
-#print(df.dropna())
-
-#list_trademarks = stn.get_words_with_trademark(stn.df_mdna)
-#df = stn.match_keywords(stn.pattern_sentiment(stn.df_mdna),list_trademarks,'trademarks')
-#print(df.dropna()[['sentence_text','pattern_sentiment','pattern_subjectivity','trademarks','trademarks_number']])
-
-#to do return full custom section blocks
-#example dividend policy, 5, df
+list_of_tickers = list(pd.read_csv('test_ticker_list.csv')['Symbol'])
+print('Running for {0} tickers...'.format(len(list_of_tickers)))
+for ticker in list_of_tickers:
+    print(ticker)
+    
+    stn = SECTextNLP(ticker)
+    
+    df = stn.get_list_sentiment(stn.df_text,GLOBAL_SEARCH_LIST,'global_macro')
+    if df is not None:
+        stn.save_to_csv(df,'global_macro')
+    
+    list_segments = stn.get_noun_phrases_around_topic(BUSINESS_SEGMENT_LIST)
+    if list_segments is not None:
+        df = stn.get_list_sentiment(stn.df_text,list_segments,'segments')
+        if df is not None:
+            stn.save_to_csv(df,'segments_sentiment')
+    
+    list_people = wiki.get_people(ticker)
+    df = stn.get_list_sentiment(stn.df_text,list_people,'people')
+    if df is not None:
+        stn.save_to_csv(df,'people_sentiment')
+    
+    list_products = stn.get_noun_phrases_around_topic(PRODUCTS_LIST)
+    if list_products is not None:
+        df = stn.get_list_sentiment(stn.df_text,list_products,'products')
+        if df is not None:
+            stn.save_to_csv(df,'products_sentiment')
+    
+    list_trademarks = stn.get_words_with_trademark(stn.df_text)
+    if list_trademarks is not None:
+        df = stn.get_list_sentiment(stn.df_text,list_trademarks,'trademarks')
+        if df is not None:
+            stn.save_to_csv(df,'trademarks_sentiment')
 
